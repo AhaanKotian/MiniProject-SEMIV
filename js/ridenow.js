5339446ef912f6fef9790d01aaf67efb5389e54a
Mohak Khetan <mmkhetan101@gmail.com>
2023-03-06 20:48:11 +0530

    improved gmap api

--- js/ridenow.js
+++ js/ridenow.js
@@ -59,7 +59,6 @@ function findOnMap(map, query){
     query: query,
     fields: ['name', 'geometry'],
   };
-  console.log(query);
 
   var service = new google.maps.places.PlacesService(map);
 
@@ -119,6 +118,46 @@ function calcRoute(directionsService, directionsRenderer) {
   directionsService.route(request, function(result, status) {
     if (status == 'OK') {
       directionsRenderer.setDirections(result);
+      distMatrix(start,end);
+
     }
   });
+}
+
+function distMatrix(origin, destination){
+  var origin1 = origin;
+  var destinationA = destination;
+
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix(
+    {
+      origins: [origin1],
+      destinations: [destinationA],
+      travelMode: 'DRIVING',
+      drivingOptions: {
+        departureTime: new Date(Date.now() + 100*60*5),  // for the time N milliseconds from now.
+        trafficModel: 'optimistic'
+      }
+    }, (response, status)=>{
+      if (status == 'OK') {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+    
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.text;
+            var duration = element.duration.text;
+            var from = origins[i];
+            var to = destinations[j];
+          }
+        }
+
+        //display above stuff
+        const dispResult = document.querySelector(".dispResult");
+        dispResult.innerHTML = "Total Distance: " + distance + "<br>" +
+                                "Duration: " + duration;
+      }
+    });
 }
