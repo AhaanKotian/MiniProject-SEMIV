e67a8de3dfbf677dfdc65991337708e84670304c
Mohak Khetan <mmkhetan101@gmail.com>
2023-04-18 22:53:23 +0530

    node basics in progress...

--- js/server.js
+++ js/server.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port ', PORT);
+})
