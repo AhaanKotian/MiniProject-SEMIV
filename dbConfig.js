5a26b943d79450cb0c7881117e826f83dac2ee82
Mohak Khetan <mmkhetan101@gmail.com>
2023-04-20 23:03:57 +0530

    code deployed on server

--- dbConfig.js
+++ dbConfig.js
@@ -9,17 +9,18 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 const connectionString2 = `postgres://mohak:pPsLrXH4zbTm986HSLj82qe8np3qgSlc@dpg-ch0im7rh4hsukp30s5ig-a.oregon-postgres.render.com/liber_lxys?ssl=true`;
 const connectionString3 = `postgres://mohak:pPsLrXH4zbTm986HSLj82qe8np3qgSlc@dpg-ch0im7rh4hsukp30s5ig-a/liber_lxys`;
 
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString3, 
-    ssl: isProduction
-});
-
 // const pool = new Pool({
-//     host: 'dpg-ch0im7rh4hsukp30s5ig-a.oregon-postgres.render.com',
-//     port: '5432',
-//     database: 'liber_lxys',
-//     user: 'mohak',
-//     password: 'pPsLrXH4zbTm986HSLj82qe8np3qgSlc'
-//   });
+//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString3, 
+//     ssl: isProduction
+// });
+
+const pool = new Pool({
+    host: 'dpg-ch0im7rh4hsukp30s5ig-a.oregon-postgres.render.com',
+    port: '5432',
+    database: 'liber_lxys',
+    user: 'mohak',
+    password: 'pPsLrXH4zbTm986HSLj82qe8np3qgSlc',
+    ssl: true
+  });
 
 module.exports = { pool };
